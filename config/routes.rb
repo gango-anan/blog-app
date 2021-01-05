9bb2c6c1d02caa3fb1d97de28f252cdddabf6133
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-06 01:22:39 +0300

    setup application home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "/articles" , to: "articles#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
