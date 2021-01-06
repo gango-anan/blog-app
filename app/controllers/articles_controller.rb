2cc2b6fe6973a7463c6a5b8710cfc61107f0b639
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-06 15:42:31 +0300

    add create new article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
 end
 
