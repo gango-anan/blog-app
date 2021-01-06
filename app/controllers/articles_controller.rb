cdf7c4cf1c9da261b402a0af8f3ff651455e422f
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-06 15:51:36 +0300

    update model with with validation functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -20,5 +20,9 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-end
 
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
