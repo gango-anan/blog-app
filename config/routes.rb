49a51e1f5571e8d299519df2e2e2eb2c969fb613
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-06 15:22:52 +0300

    update articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "/articles", to: "articles#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/articles/:id", to: "articles#show"
 end
+
